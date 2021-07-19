db3fbe4979b5b0ca82bed4553f788e320b418747
CatB1t <71686115+CatB1t@users.noreply.github.com>
2021-07-19 20:19:56 +0200

    Handled file not found for reading input

--- UserManager.cpp
+++ UserManager.cpp
@@ -14,53 +14,65 @@ UserManager::UserManager()
         UserManager_Instance = this;
 
     ifstream nameFileout;
-    nameFileout.open(R"(../all-users.in)"); // TODO handle if file is not found
-    while(!nameFileout.eof()){
-        string userName;
-        string name;
-        string email;
-        nameFileout >> userName;
-        userName.erase(userName.end() - 1);
-        getline(nameFileout, name, ',');
-        name.erase(name.begin());
-        nameFileout >> email;
-        User user(userName, name, email);
-        if(_userList.search(userName))
-        {
-            std::cout << "ERROR! user name already exists" << endl;
-            continue;
-        }
-        _userList.insertAtHead(user);
-    };
+    nameFileout.open(ALL_USERS_PATH);
 
-    nameFileout.close();
+    if(!nameFileout.is_open())
+    {
+        std::cout << "Couldn't open file ('all-users.in'), please check UserManager.h for the correct path" << std::endl;
+    }
+    else {
+        while(!nameFileout.eof()){
+            string userName;
+            string name;
+            string email;
+            nameFileout >> userName;
+            userName.erase(userName.end() - 1);
+            getline(nameFileout, name, ',');
+            name.erase(name.begin());
+            nameFileout >> email;
+            User user(userName, name, email);
+            if(_userList.search(userName))
+            {
+                std::cout << "ERROR! user name already exists" << endl;
+                continue;
+            }
+            _userList.insertAtHead(user);
+        };
+        nameFileout.close();
+    }
+
     ReadInputForRelations();
 }
 
 void UserManager::ReadInputForRelations()
 {
     ifstream tmp;
-    tmp.open("../all-users-relations.in");
+    tmp.open(ALL_USERS_RELATIONS_IN);
 
-    while(!tmp.eof())
+    if(!tmp.is_open())
     {
-        string tmpFirstUser,tmpSecondUser;
-
-        tmp >> tmpFirstUser;
-        tmpFirstUser.erase(tmpFirstUser.end() - 1);
-        tmp >> tmpSecondUser;
-
-        User* firstUser = _userList.search(tmpFirstUser);
-        User* secondUser = _userList.search(tmpSecondUser);
-
-        if(firstUser && secondUser)
-        {
-            firstUser->AddFriend(*secondUser);
-            secondUser->AddFriend(*firstUser);
-        }
+        std::cout << "Couldn't open file ('all-users-relations.in'), please check UserManager.h for the correct path" << std::endl;
     }
+    else {
+        while(!tmp.eof())
+        {
+            string tmpFirstUser,tmpSecondUser;
 
-    tmp.close();
+            tmp >> tmpFirstUser;
+            tmpFirstUser.erase(tmpFirstUser.end() - 1);
+            tmp >> tmpSecondUser;
+
+            User* firstUser = _userList.search(tmpFirstUser);
+            User* secondUser = _userList.search(tmpSecondUser);
+
+            if(firstUser && secondUser)
+            {
+                firstUser->AddFriend(*secondUser);
+                secondUser->AddFriend(*firstUser);
+            }
+        }
+        tmp.close();
+    }
 }
 
 User* UserManager::Login(const std::string& name)
--- UserManager.h
+++ UserManager.h
@@ -16,13 +16,13 @@ private:
     UserLinkedList _userList;
     void ReadInputForRelations();
 public:
+    const char* const ALL_USERS_PATH = "../all-users.in";
+    const char* const ALL_USERS_RELATIONS_IN = "../all-users-relations.in";
     static UserManager* UserManager_Instance;
 public:
     UserManager();
     User* Login(const std::string& name);
     User* Search(const std::string& name);
-
-
 
     UserLinkedList GetUserRecommendation(User& user)
     {
